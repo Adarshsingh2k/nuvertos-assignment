112e54835145ab8422a88c0b4efd07cb40840b4c
Adarsh <adarshsingh2k@gmail.com>
2025-01-08 22:42:48 +0530

    done

--- src/enviroment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: '570b5124534b20f59c0af0367b488dba',
-};
--- src/enviroment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-
-  apiKey: '570b5124534b20f59c0af0367b488dba',
-};
